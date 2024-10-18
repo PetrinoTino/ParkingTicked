a419313893e09e26b7209f4ce0d0d15409d440e4
Christian Lico <christianlico2001@gmail.com>
2024-10-18 22:45:34 +0200

    parking section

--- src/main/java/com/sda/ParkingSection.java
+++ src/main/java/com/sda/ParkingSection.java
@@ -4,30 +4,67 @@ package com.sda;
 import java.time.Duration;
 
     public class ParkingSection {
-        private long id;
-        private LocalDateTime entryTime;
-        private LocalDateTime exitTime;
+        private int id;
         private String licensePlate;
+        private int slotId;
+        private int entryHour;
+        private int exitHour;
 
-        public ParkingSection(long id, String licensePlate) {
+        public ParkingSection(int id, String licensePlate, int slotId, int entryHour,int exitHour) {
             this.id = id;
             this.licensePlate = licensePlate;
-            this.entryTime = LocalDateTime.now();
+            this.slotId = slotId;
+            this.entryHour = this.entryHour;
+            this.exitHour = -1;
         }
 
-        public void setExitTime(LocalDateTime exitTime) {
-            this.exitTime = exitTime;
+        public ParkingSection(int id, String licensePlate, int slotId) {
         }
 
-        public Duration calculateDuration() {
-            if (exitTime == null) {
-                return Duration.between(entryTime, LocalDateTime.now());
-            }
-            return Duration.between(entryTime, exitTime);
+        public int getId() {
+            return id;
         }
 
         public String getLicensePlate() {
             return licensePlate;
+        }
+
+        public int getSlotId() {
+            return slotId;
+        }
+
+        public int getEntryHour() {
+            return entryHour;
+        }
+
+        public int getExitHour() {
+            return exitHour;
+        }
+        public void setExitHour(int exitHour) {
+            if (exitHour < entryHour) {
+                throw new IllegalArgumentException("Exit hour cannot be before entry hour");
+            }
+            this.exitHour = exitHour;
+        }
+        public int calculateHoursParked() {
+            if (exitHour == -1) {
+                throw new IllegalStateException("Car has not exited yet");
+            }
+            return exitHour - entryHour;
+        }
+        public double calculateFee(double hourlyRate) {
+            int hoursParked = calculateHoursParked();
+            return hoursParked * hourlyRate;
+        }
+        @Override
+        public String toString() {
+            return "ParkingSection{" +
+                    "id=" + id +
+                    ", licensePlate='" + licensePlate + '\'' +
+                    ", slotId=" + slotId +
+                    ", entryHour=" + entryHour +
+                    ", exitHour=" + (exitHour == -1 ? "Still parked" : exitHour) +
+                    '}';
         }
     }
 
