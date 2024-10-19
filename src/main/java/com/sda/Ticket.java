82196c7f7c98bf6127578515799a89ecc69ae4e0
Enea Kula <kulaenea94@gmail.com>
2024-10-19 12:57:04 +0200

    Parking slot 2.1

--- src/main/java/com/sda/Ticket.java
+++ src/main/java/com/sda/Ticket.java
@@ -1,49 +1,60 @@
 package com.sda;
 
+
+
 import java.time.Duration;
 
-public class Ticket {
-    private final int id;
-    private final double price;
+    public class Ticket {
+        private int id;
+        private double price;
+        private Duration duration;
+        private String customerName;
+        private boolean isMember;
 
-    private final String costumerName;
-    private final boolean isMember;
+        public Ticket(int id, Duration duration, String customerName, boolean isMember) {
+            this.id = id;
+            this.duration = duration;
+            this.customerName = customerName;
+            this.isMember = isMember;
+            this.price = calculatePrice();
+        }
 
-    public Ticket(int id, double price, String costumerName, boolean isMember) {
-        this.id = id;
-        this.price = price;
-        this.costumerName = costumerName;
-        this.isMember = isMember;
+        public int getId() {
+            return id;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public Duration getDuration() {
+            return duration;
+        }
+
+        public String getCustomerName() {
+            return customerName;
+        }
+
+        public boolean isMember() {
+            return isMember;
+        }
+
+        private double calculatePrice() {
+            double basePrice = duration.toHours() * 5.0; // $5 per hour
+            if (isMember) {
+                return basePrice * 0.8; // 20% discount for members
+            }
+            return basePrice;
+        }
+
+        @Override
+        public String toString() {
+            return "Ticket{" +
+                    "id=" + id +
+                    ", price=" + price +
+                    ", duration=" + duration +
+                    ", customerName='" + customerName + '\'' +
+                    ", isMember=" + isMember +
+                    '}';
+        }
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public Duration getDuration() {
-        return duration;
-    }
-
-    public String getCostumerName() {
-        return costumerName;
-    }
-
-    public boolean isMember() {
-        return isMember;
-    }
-
-    @Override
-    public String toString() {
-        return "Ticket{" +
-                "id=" + id +
-                ", price=" + price +
-                ", duration=" + duration +
-                ", costumerName='" + costumerName + '\'' +
-                ", isMember=" + isMember +
-                '}';
-    }
-}
